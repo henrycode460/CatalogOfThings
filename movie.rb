6588e86524ff1a718450d3a53658986b2e14f588
Kamba56 <tomajohn316@yahoo.com>
2022-09-20 16:32:06 +0100

    Fix linter errors

--- movie.rb
+++ movie.rb
@@ -2,8 +2,13 @@ require './item'
 
 class Movie < Item
   attr_accessor :silet
+  
   def initialize(silet)
     super()
     @silet = silet
+  end
+
+  def can_be_archived?
+    (Date.now - Date.parse(@publish_date)) / 365 > 10 || silet
   end
 end
