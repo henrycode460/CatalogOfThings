cc915862192929e5007f7c8d4452a5fbfcb8d3ff
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-09-21 00:32:55 +0100

    Add game method

--- game.rb
+++ game.rb
@@ -1,18 +1,16 @@
-require_relative './item'
-require_relative './date'
-class Games < Item
-
+require './item'
+class Game < Item
   attr_accessor :multi_player, :last_played_at, :publish_date
-  def initialize(multi_player, last_played_at, publish_date)
+
+  def initialize(multi_player, last_played_at, _publish_date)
+    super(publish_date)
     @multi_player = multi_player
     @last_played_at = last_played_at
   end
 
   def can_be_archived?
     super && (Date.today.year - last_played_at.year) > 2
-
   end
 
   private :can_be_archived?
-
 end
