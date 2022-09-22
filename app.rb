c3dcf4aa23449afb0a2be851a17314c362aab9f9
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-09-22 18:42:50 +0100

    Change relative path for json files

--- app.rb
+++ app.rb
@@ -2,13 +2,22 @@ require './classes/item'
 require './classes/genre'
 require './classes/musicalbum'
 require_relative 'storage'
+require './classes/game'
+require './classes/author'
+require_relative 'store_game'
+require_relative 'store_author'
+
 
 class App
   include Storage
+  include GamesPreserve
+  include AuthorsPreserve
 
   def initialize
     @genre = []
     @musicalbums = []
+    @games = []
+    @authors = []
   end
 
   def list_all_music_album
@@ -42,10 +51,93 @@ class App
     @musicalbums.push(MusicAlbum.new(name, publish_date, on_spotify))
   end
 
+  # Add_game
+  def add_game
+    print 'Publish date [YYYY-MM-DD] : '
+    publish_date = gets.chomp
+
+    print 'Is multiplayer game? [ yes, no ]: '
+    multiplayer = gets.chomp
+
+    case multiplayer
+    when 'yes'
+      multiplayer = 'yes'
+    when 'no'
+      multiplayer = 'no'
+    else
+      puts 'yes or no'
+      multiplayer = gets.chomp
+    end
+
+    print 'is Archived [Y/N]: '
+    archived = gets.chomp.downcase
+
+    case archived
+    when 'y'
+      archived = true
+
+    when 'n'
+      archived = false
+    else
+      puts('please enter [Y/N]: ')
+    end
+
+    print 'Last played at[YYYY-MM-DD]: '
+    last_played_at = gets.chomp
+
+    print 'Author First Name: '
+    first_name = gets.chomp
+
+    print 'Author Last Name: '
+    last_name = gets.chomp
+    author = Author.new(first_name, last_name)
+    @authors.push(author)
+    game = Game.new(publish_date, multiplayer, last_played_at, archived, first_name)
+    @games.push(game)
+
+    author.add_item(game)
+    game.add_author(author)
+
+    puts 'New Game added successfully'
+  end
+
+  # List game and author
+
+  def list_authors
+    if @authors.empty?
+      puts 'No author in the catalog'
+      return
+    end
+    puts "\n"
+
+    @authors.each_with_index do |author, i|
+      puts "#{i + 1}) #{author.first_name} #{author.last_name}"
+      puts
+    end
+  end
+
+  def list_games
+    if @games.empty?
+      puts 'No game in the catalog'
+      return
+    end
+    puts "\n"
+
+    @games.each do |game|
+      print "Author: #{game.first_name},
+             publish date: #{game.publish_date},
+             multiplayer: #{game.multiplayer},
+             last_played_at: #{game.last_played_at}"
+      puts
+    end
+  end
+
   # saves data at the exit of the app
   def quit
     save_state
     save_genre
+    store_games
+    store_authors
   end
 
   # loads the saved data on the start of the app
