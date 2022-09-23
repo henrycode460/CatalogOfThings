6325b3b2e98122d3febcf37e1d86b708d6faec1d
Kamba56 <tomajohn316@yahoo.com>
2022-09-23 12:07:14 +0100

    Add game and author table to schema.sql

--- schema.sql
+++ schema.sql
@@ -1,32 +1,75 @@
 CREATE DATABASE Catalog;
 
-CREATE TABLE MusicAlbum(
-ID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-publish_date DATE,
-archived BOOLEAN
-);
-
 CREATE TABLE genre(
-ID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-name TEXT
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT
 );
 
-ALTER TABLE musicalbum ADD COLUMN
-genre_id INT REFERENCES genre(id
+CREATE TABLE source(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(200),
+  PRIMARY KEY(id)
+);
 
-CREATE TABLE book (
-   id SERIAL PRIMARY KEY,
-   publisher VARCHAR(50) NOT NULL,
-    cover_state INT NOT NULL,
-    
+CREATE TABLE author(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(200),
+  last_name VARCHAR(200),
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE label (
-   id SERIAL PRIMARY KEY,
-   title VARCHAR(50) NOT NULL,
-   color VARCHAR(50) NOT NULL,
-   item_id int 
-   
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  color VARCHAR(50) NOT NULL,
+  item_id int    
 );
 
-ALTER TABLE label ADD CONSTRAINT fk_constraint FOREIGN KEY (item_id) REFERENCES item (id);
+CREATE TABLE MusicAlbum(
+  ID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT,
+  source_id INT REFERENCES source(id),
+  genre_id INT REFERENCES genre(id),
+  author_id INT REFERENCES author(id),
+  label_id INT REFERENCES label(id),
+  publish_date DATE,
+  archived BOOLEAN,
+  on_spotify BOOLEAN
+);
+
+CREATE TABLE movie(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(300),
+  source_id INT REFERENCES source(id),
+  genre_id INT REFERENCES genre(id),
+  author_id INT REFERENCES author(id),
+  label_id INT REFERENCES label(id),
+  publishe_date DATE,
+  archived BOOLEAN,
+  silet BOOLEAN,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE book (
+  ID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  publisher TEXT,
+  cover_state TEXT,
+  source_id INT REFERENCES source(id),
+  genre_id INT REFERENCES genre(id),
+  author_id INT REFERENCES author(id),
+  label_id INT REFERENCES label(id),
+  publish_date DATE,
+  archived BOOLEAN
+);
+
+CREATE TABLE games (
+  ID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  source_id INT REFERENCES source(id),
+  genre_id INT REFERENCES genre(id),
+  author_id INT REFERENCES author(id),
+  label_id INT REFERENCES label(id),
+  publish_date DATE,
+  last_played_at DATE,
+  archived BOOLEAN,
+  multiplayer BOOLEAN
+);
